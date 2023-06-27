daa0a5e31dd497c20820b23f5fee4b5a91264531
Agustinaacost4 <agustinaderek09@gmail.com>
2023-06-27 03:40:34 -0300

    tp terminado

--- frontend/js/formulario.js
+++ frontend/js/formulario.js
@@ -1,44 +1,148 @@
-const $ = (id) => document.getElementById(id)
-const urlBase = "http://localhost:3031/api/movies/"
-
+const $ = (id) => document.getElementById(id);
+const qs = (id) => document.querySelector(id);
+const urlBase = 'http://localhost:3031/api/movies/'
 window.onload = async () => {
 
-    let query = new URLSearchParams(location.search);
-    let id = query.has("id") && query.get("id");
+    let query = new URLSearchParams(location.search)
+   
+    const id = query.has('id') && query.get('id')
+    
+    if (!id) {
+        qs('button').setAttribute('hidden', 'hidden')
+        qs('button.botonBorrar').setAttribute('hidden', 'hidden')
+        qs('h1').innerText = 'Creando pelicula nueva'
 
-    try {
-        let response = await fetch(urlBase + id)
-        let pelicula = await response.json()
-        let{title, rating, awards, length = duration, duration, release_date} = pelicula.data
-        
-        $("title").value = title,
-        $("rating").value = rating,
-        $("awards").value = awards,
-        $("length").value = length,
-        $("release_date").value =moment(release_date).format("YYYY-MM-DD")
-    } catch (error) {
-        
-    }
-        document.querySelector(".formulario").addEventListener("submit", async(e) => {
+        $("crear").addEventListener("click", async (e) => {
             e.preventDefault();
             try {
-                let response = await fetch(urlBase + "update/" + id, {
-                    method: "PUT",
-                    body : JSON.stringify({
-                        title : $("title").value,
-                        rating : $("rating").value,
-                        awards : $("awards").value,
-                        length : $("length").value,
-                        release_date : $("release_date").value
+
+                let response = await fetch("http://localhost:3031/api/movies/create", {
+                    method: "POST",
+                    body: JSON.stringify({
+                        title: $("title").value,
+                        rating: $("rating").value,
+                        awards: $("awards").value,
+                        release_date: $("release_date").value,
+                        length: $("length").value,
                     }),
-                    headers : {
-                        "Content-Type" : "application/json"
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                })
+
+                let result = await response.json();
+                console.log(result);
+
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Película creada',
+                    text: 'La película se ha creado exitosamente.',
+                }).then(() => {
+                   
+                    window.location.href = 'home.html';
+                });
+
+            } catch (error) {
+                console.error;
+            }
+
+        })
+    } 
+    else {
+        $('crear').setAttribute('hidden', 'hidden')
+        /* EDICION */
+        try {
+
+          
+            let response = await fetch(urlBase + id)
+            let pelicula = await response.json()
+            //console.log(pelicula);
+
+            let { title, rating, awards, length: duracion, release_date } = pelicula.data;
+            console.log(title);
+
+            qs('h1').innerText = `Estas editando la pelicula: ${title}`
+           
+            let formatReleaseDate = moment(release_date).format('YYYY-MM-DD')
+
+            document.querySelector("#title")
+            $('title').value = title;
+            $('rating').value = rating;
+            $('awards').value = awards;
+            $('length').value = duracion;
+            $('release_date').value = formatReleaseDate;
+
+
+        } catch (error) {
+            console.log(error);
+        }
+        qs('button').addEventListener('click', async (e) => {
+            e.preventDefault();
+            try {
+
+                response = await fetch(urlBase + 'update/' + id, {
+                    method: 'PUT',
+                    body: JSON.stringify({
+                        title: $('title').value,
+                        rating: $('rating').value,
+                        awards: $('awards').value,
+                        length: $('length').value,
+                        release_date: $('release_date').value,
+
+                    }),
+                    headers: { 'Content-Type': 'application/json' }
+
+                })
+                let result = await response.json();
+                console.log(result);
+
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Película modificada',
+                    text: 'La película se ha modificado exitosamente.',
+                }).then(() => {
+                  
+                    window.location.href = 'home.html';
+                });
+
+            } catch (error) {
+                console.log(error);
+            }
+            
+
+        })
+        /* ELIMINACION */
+        qs('button.botonBorrar').addEventListener('click', async (e) => {
+            e.preventDefault();
+
+            try {
+                Swal.fire({
+                    title: "¿Estás seguro de que deseas eliminar esta película?",
+                    icon: "warning",
+                    showCancelButton: true,
+                    confirmButtonText: "Sí, eliminar",
+                    cancelButtonText: "Cancelar"
+                }).then(async (result) => {
+                    if (result.isConfirmed) {
+                        try {
+                            await fetch(urlBase + 'delete/' + id, {
+                                method: 'DELETE',
+                                headers: { 'Content-Type': 'application/json' }
+                            });
+
+                            Swal.fire("Película eliminada", "", "success").then(() => {
+                                
+                                window.location.href = 'home.html';
+                            });
+                        } catch (error) {
+                            console.log(error);
+                        }
                     }
                 });
-                let result = await response.json()
-                console.log(result);
             } catch (error) {
-                console.error(error);
+                console.log(error);
             }
-        })
+        });
+        
+    }
 }
--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,14 +1,28 @@
-window.onload = async() => {
+const $ = (id) => document.getElementById(id);
+const qs = (id) => document.querySelector(id);
+window.onload = async () => {
   const app = document.getElementById("root");
+
+  const favorites = localStorage.getItem("favorites") ? JSON.parse(localStorage.getItem("favorites")) : [];
+  const container1 = document.querySelector('.container')
+  
+  if (favorites.length > 0) {
+    const favoritesButton = document.createElement('a')
+    favoritesButton.setAttribute('class', 'botonAgregar')
+    favoritesButton.setAttribute('href', 'favoritas.html')
+    favoritesButton.setAttribute('id', 'favoritesButton')
+    favoritesButton.innerText = 'Ver favoritos'
+    container1.appendChild(favoritesButton)
+  }
   const container = document.createElement("div");
   container.setAttribute("class", "container");
   app.appendChild(container);
-
-  // Aqui debemos agregar nuestro fetch
+  console.log(favorites);
+ 
   try {
-    let response = await fetch("http://localhost:3031/api/movies");
-    let peliculas = await response.json();
-
+    let response = await fetch('http://localhost:3031/api/movies')
+    let peliculas = await response.json()
+    
     let data = peliculas.data;
 
     data.forEach((movie) => {
@@ -21,8 +35,58 @@ window.onload = async() => {
       const p = document.createElement("p");
       p.textContent = `Rating: ${movie.rating}`;
 
-      const duracion = document.createElement("p");
-      duracion.textContent = `Duración: ${movie.length}`;
+     
+      const editButton = document.createElement("a")
+      editButton.textContent = 'Modificar'
+      editButton.setAttribute('href', `formulario.html?id=${movie.id}`)
+      editButton.setAttribute('class', 'botonModificar')
+
+      
+      const star = document.createElement('i')
+      if (!favorites.includes(movie.id)) {
+        star.setAttribute('class', "far fa-star favStar")
+      } else {
+        star.setAttribute('class', "fas fa-star favStar favStarFlip")
+      }
+      star.setAttribute('id', `${movie.id}`)
+
+   
+      star.addEventListener("click", () => {
+
+       
+        if (!$('favoritesButton')) {
+
+          const favoritesButton = document.createElement('a')
+          favoritesButton.setAttribute('class', 'botonAgregar')
+          favoritesButton.setAttribute('id', 'favoritesButton')
+          favoritesButton.setAttribute('href', 'favoritas.html')
+          favoritesButton.innerText = 'Ver favoritos'
+          container1.appendChild(favoritesButton)
+        }
+
+        console.log(`hice click en la estrella ${movie.id}`);
+
+        star.classList.toggle("far");
+        star.classList.toggle("favStarFlip");
+        star.classList.toggle("fas");
+
+        
+        if (!favorites.includes(movie.id)) {
+          favorites.push(movie.id);
+          localStorage.setItem("favorites", JSON.stringify(favorites));
+          console.log(localStorage.favorites);
+        } 
+        else {
+          const index = favorites.indexOf(movie.id)
+          favorites.splice(index, 1)
+          localStorage.setItem("favorites", JSON.stringify(favorites));
+          console.log(localStorage.favorites);
+          //si no tengo mas peliculas en favoritos y el boton de favoritos existe lo quito con remove
+          if (favorites.length === 0 && favoritesButton) {
+            favoritesButton.remove();
+          }
+        }
+      })
 
       container.appendChild(card);
       card.appendChild(h1);
@@ -32,23 +96,16 @@ window.onload = async() => {
         genero.textContent = `Genero: ${movie.genre.name}`;
         card.appendChild(genero);
       }
-      card.appendChild(duracion);
-      const a = document.createElement("a");
-      a.setAttribute("href", "formulario.html?id=" + movie.id);
-      a.setAttribute("class", "botonAgregar");
-      a.textContent = "Ver más";
-      card.appendChild(a)
-
-      //Estrella de favoritos:
-      const star = document.createElement("a");
-      star.setAttribute("class", "botonAgregar");
-      star.innerHTML = '<i class="fa-regular fa-star fa-beat" style="color: #f6fa00;"></i>';
-      star.setAttribute("id", "movie" + movie.id)
+      if (movie.length !== null) {
+        const duracion = document.createElement("p");
+        duracion.textContent = `Duración: ${movie.length}`;
+        card.appendChild(duracion);
+      }
+      card.appendChild(editButton)
       card.appendChild(star)
     });
-
   } catch (error) {
-    console.error
+    console.log(error);
   }
 
 
